1fe85de74699e999d459391b332a57b840bf6a9b
Lwam Hailu <lwi25@yahoo.com>
2017-11-13 10:58:52 -0600

    spring AOP

---
+++ AOPExamples/src/com/edu/service/ShapeService.java
@@ -0,0 +1,26 @@
+package com.edu.service;
+
+import com.edu.model.Circle;
+import com.edu.model.Triangle;
+
+public class ShapeService {
+	private Circle circle;
+	private Triangle triangle;
+
+	public Circle getCircle() {
+		return circle;
+	}
+
+	public void setCircle(Circle circle) {
+		this.circle = circle;
+	}
+
+	public Triangle getTriangle() {
+		return triangle;
+	}
+
+	public void setTriangle(Triangle triangle) {
+		this.triangle = triangle;
+	}
+
+}
